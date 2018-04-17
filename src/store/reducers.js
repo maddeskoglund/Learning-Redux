b427f1be22258d47187ba3638aa22b80af1d4eae
maddeskoglund <madelein.blomster@hotmail.com>
2018-04-17 14:51:24 +0200

    Ch 2.4
    Create array reducers

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -21,3 +21,16 @@ export const skiDay = (state = null, action) =>
 //     return state;
 //   }
 // };
+
+export const errors = (state = [], action) => {
+  switch (action.type) {
+    case C.ADD_ERROR:
+      return [...state, action.payload];
+
+    case C.CLEAR_ERROR:
+      return state.filter((message, i) => i !== action.payload);
+
+    default:
+      return state;
+  }
+};
