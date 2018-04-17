f9df4f3248d6d4f9f1a5a9c4be7a53838c9fa7e0
maddeskoglund <madelein.blomster@hotmail.com>
2018-04-17 14:14:06 +0200

    Ch 2.3
    Create Object reducers

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,9 +1,23 @@
 import C from "../constants";
 
-export const goal = (state = 10, action) => {
-  if (action.type === C.SET_GOAL) {
-    return parseInt(action.payload);
-  } else {
-    return state;
-  }
-};
+export const goal = (state = 10, action) =>
+  action.type === C.SET_GOAL ? parseInt(action.payload) : state;
+
+// {
+//   if (action.type === C.SET_GOAL) {
+//     return parseInt(action.payload);
+//   } else {
+//     return state;
+//   }
+// };
+
+export const skiDay = (state = null, action) =>
+  action.type === C.ADD_DAY ? action.payload : state;
+
+// export const skiDay = (state = null, action) => {
+//   if (action.type === C.ADD_DAY) {
+//     return action.payload;
+//   } else {
+//     return state;
+//   }
+// };
